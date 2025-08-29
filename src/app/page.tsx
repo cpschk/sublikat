83903084b3caffccbfda743b8cc701e320ef39da
Cristian Renaud Peschke Salcedo <absurdania.rpg@gmail.com>
2025-08-29 17:03:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ScrollStack, {
   ScrollStackItem,
 } from '@/components/scroll-stack';
-import './scroll-stack.css';
+import '@/components/scroll-stack.css';
 
 export default function SublikatWireframe() {
   return (
